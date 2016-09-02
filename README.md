3cd440927cdc8636e5f18a84d87a20040e6e2954
Bo Zhang <bozhang@nao.cas.cn>
2016-09-02 12:30:17 +0800

    update README & setup.py

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # TheKeenan
+A forward model to estimate stellar parameters (e.g., Teff, logg, [Fe/H] and chemical abundances) using Support Vector Regression (SVR), which is a non-parametric method.
+
+# Installation
+`pip install TheKeenan`
+
+# Requirements
+`LIBSVM`
---
+++ setup.py
@@ -0,0 +1,34 @@
+from distutils.core import setup
+
+
+if __name__ == '__main__':
+    setup(
+        name='TheKeenan',
+        version='0.0.1',
+        author='Bo Zhang',
+        author_email='bozhang@nao.cas.cn',
+        description='A forward model using SVR to estimate stellar parameters from spectra.',  # short description
+        license='MIT',
+        # install_requires=['numpy>=1.7','scipy','matplotlib','nose'],
+        url='http://github.com/hypergravity/TheKeenan',
+        classifiers=[
+            "Development Status :: 6 - Mature",
+            "Intended Audience :: Science/Research",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+            "Programming Language :: Python :: 2.7",
+            "Topic :: Scientific/Engineering :: Astronomy",
+            "Topic :: Scientific/Engineering :: Physics"],
+        package_dir={'TheKeenan': 'TheKeenan'},
+        packages=['TheKeenan'],
+        # package_data={'starlight_wrapper': ['data/SDSS_DR10/*',
+        #                                     'data/SDSS_DR10/0603/*',
+        #                                     'data/executable/*',
+        #                                     'data/template_mask/*',
+        #                                     'data/template_base/*',
+        #                                     'data/template_base/Base.BC03/*',
+        #                                     'data/template_base/Base.SED.FWHM2.5/*',
+        #                                     'data/template_base/Base.SED.FWHM0.3/*']},
+        # include_package_data=True,
+        requires=['numpy', 'scipy', 'matplotlib', 'astropy', 'bopy']
+    )
