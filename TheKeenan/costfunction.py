5646c70581cec50892beca74c9822e7f2413053e
Bo Zhang <bozhang@nao.cas.cn>
2016-09-03 13:01:51 +0800

    add costfunction.py

---
+++ TheKeenan/costfunction.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+
+Author
+------
+Bo Zhang
+
+Email
+-----
+bozhang@nao.cas.cn
+
+Created on
+----------
+- Sat Sep 03 12:00:00 2016
+
+Modifications
+-------------
+- Sat Sep 03 12:00:00 2016
+
+Aims
+----
+- define cost functions
+ - temporally only chi2 is implemented
+
+"""
+
+from __future__ import division
+
+import numpy as np
+from scipy.stats import chisquare
+
+
+def chi2_simple_1d(spec_obs, spec_pred, ivar=None):
+    """ Calculate ivar-weighted chi-square for two spectra """
+    if ivar is not None:
+        return np.sum((np.array(spec_obs).flatten() - np.array(
+            spec_pred).flatten()) ** 2.)
+    else:
+        return np.sum((np.array(spec_obs).flatten() - np.array(
+            spec_pred).flatten()) ** 2. * np.array(ivar).flatten())
+
+
+def chi2(a, b):
+    c = chisquare([1, 2, 3], [1, 1, 1])
+    return c.statistic
