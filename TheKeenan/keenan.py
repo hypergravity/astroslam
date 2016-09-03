b51636dbec4fae1520b129555a7e142ba3a4290d
Bo Zhang <bozhang@nao.cas.cn>
2016-09-03 14:15:44 +0800

    add keenan.py

---
+++ TheKeenan/keenan.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+
+Author
+------
+Bo Zhang
+
+Email
+-----
+bozhang@nao.cas.cn
+
+Created on
+----------
+- Sat Sep 03 12:00:00 2016
+
+Modifications
+-------------
+- Sat Sep 03 12:00:00 2016
+
+Aims
+----
+- Keenan class
+
+"""
+
+
+class Keenan(object):
+    """ This defines """
+    svrs = []
+    n_pix = len(svrs)
+
+    def __init__(self):
+        pass
+
+    def __refresh_repr__(self):
+        pass
+
+    def __repr__(self):
+        pass
