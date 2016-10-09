52d3685fbb9cf31bfdb9375d67beb009bb71c5c3
Bo Zhang <bozhang@nao.cas.cn>
2016-10-09 20:09:54 +0800

    update setup.py: update version to 0.5.0 (rename TheKeenan to slam)

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@ from distutils.core import setup
 
 if __name__ == '__main__':
     setup(
-        name='TheKeenan',
-        version='0.4.0',
+        name='slam',
+        version='0.5.0',
         author='Bo Zhang',
         author_email='bozhang@nao.cas.cn',
         description=('A forward model using SVR to estimate stellar parameters'
                      ' from spectra.'),  # short description
         license='MIT',
         # install_requires=['numpy>=1.7','scipy','matplotlib','nose'],
-        url='http://github.com/hypergravity/TheKeenan',
+        url='http://github.com/hypergravity/slam',
         classifiers=[
             "Development Status :: 6 - Mature",
             "Intended Audience :: Science/Research",
@@ -21,19 +21,17 @@ if __name__ == '__main__':
             "Programming Language :: Python :: 3.5",
             "Topic :: Scientific/Engineering :: Astronomy",
             "Topic :: Scientific/Engineering :: Physics"],
-        package_dir={'TheKeenan': 'TheKeenan',
-                     'extern': 'TheKeenan/extern'},
-        packages=['TheKeenan',
-                  'TheKeenan/extern'],
-        # package_data={'starlight_wrapper': ['data/SDSS_DR10/*',
-        #                                     'data/SDSS_DR10/0603/*',
-        #                                     'data/executable/*',
-        #                                     'data/template_mask/*',
-        #                                     'data/template_base/*',
-        #                                     'data/template_base/Base.BC03/*',
-        #                                     'data/template_base/Base.SED.FWHM2.5/*',
-        #                                     'data/template_base/Base.SED.FWHM0.3/*']},
+        package_dir={'slam': 'slam',
+                     'extern': 'slam/extern'},
+        packages=['slam',
+                  'slam/extern'],
         # include_package_data=True,
-        requires=['numpy', 'scipy', 'matplotlib', 'astropy',
-                  'sklearn', 'joblib', 'pandas', 'emcee']
+        requires=['numpy',
+                  'scipy',
+                  'matplotlib',
+                  'astropy',
+                  'sklearn',
+                  'joblib',
+                  'pandas',
+                  'emcee']
     )
