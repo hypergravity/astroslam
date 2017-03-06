5e71e04e4792e3b0a9fc30c29cb7138ba113f301
Bo Zhang <bozhang@nao.cas.cn>
2017-03-06 19:35:08 +0800

    update apogee: update downloading routines (for python3)

--- slam/apogee.py
+++ slam/apogee.py
@@ -27,30 +27,31 @@ from __future__ import print_function
 
 import os
 import urllib
+import urllib.request
 from collections import OrderedDict
 
 import numpy as np
 from astropy.io import fits
 from astropy.table import Table, Column
 
-__all__ = ['apStar_url', 'apStar_download', 'mkdir_loop']
+__all__ = ["apStar_url", "apStar_download", "mkdir_loop"]
 
 
 def reconstruct_wcs_coord_from_fits_header(hdr, dim=1):
     """ reconstruct wcs coordinates (e.g., wavelength array) """
     # assert dim is not larger than limit
-    assert dim <= hdr['NAXIS']
+    assert dim <= hdr["NAXIS"]
 
     # get keywords
-    crval = hdr['CRVAL%d' % dim]
-    cdelt = hdr['CDELT%d' % dim]
+    crval = hdr["CRVAL%d" % dim]
+    cdelt = hdr["CDELT%d" % dim]
     try:
-        crpix = hdr['CRPIX%d' % dim]
+        crpix = hdr["CRPIX%d" % dim]
     except KeyError:
         crpix = 1
 
     # length of the current dimension
-    naxis_ = hdr['NAXIS%d' % dim]
+    naxis_ = hdr["NAXIS%d" % dim]
 
     # reconstruct wcs coordinates
     coord = np.arange(1 - crpix, naxis_ + 1 - crpix) * cdelt + crval
@@ -103,23 +104,23 @@ def apStar_read(fp, full=False, meta=False, verbose=False):
         spec = Table([
             Column(
                 10. ** reconstruct_wcs_coord_from_fits_header(hl[1].header, 1),
-                'wave'),
-            Column(hl[1].data.T, 'flux'),
-            Column(hl[2].data.T, 'flux_err'),
-            Column(hl[3].data.T, 'mask')])
+                "wave"),
+            Column(hl[1].data.T, "flux"),
+            Column(hl[2].data.T, "flux_err"),
+            Column(hl[3].data.T, "mask")])
     else:
         # full apStar info
         spec = Table([
             Column(
                 10. ** reconstruct_wcs_coord_from_fits_header(hl[1].header, 1),
-                'wave'),
-            Column(hl[1].data.T, 'flux'),
-            Column(hl[2].data.T, 'flux_err'),
-            Column(hl[3].data.T, 'mask'),
-            Column(hl[4].data.T, 'sky'),
-            Column(hl[5].data.T, 'sky_err'),
-            Column(hl[6].data.T, 'telluric'),
-            Column(hl[7].data.T, 'telluric_err')])
+                "wave"),
+            Column(hl[1].data.T, "flux"),
+            Column(hl[2].data.T, "flux_err"),
+            Column(hl[3].data.T, "mask"),
+            Column(hl[4].data.T, "sky"),
+            Column(hl[5].data.T, "sky_err"),
+            Column(hl[6].data.T, "telluric"),
+            Column(hl[7].data.T, "telluric_err")])
 
     # meta data
     if meta:
@@ -166,10 +167,10 @@ def aspcapStar_read(fp, meta=False, verbose=False):
     spec = Table([
         Column(
             10. ** reconstruct_wcs_coord_from_fits_header(hl[1].header, 1),
-            'wave'),
-        Column(hl[1].data, 'flux'),
-        Column(hl[2].data, 'flux_err'),
-        Column(hl[3].data, 'flux_fit')])
+            "wave"),
+        Column(hl[1].data, "flux"),
+        Column(hl[2].data, "flux_err"),
+        Column(hl[3].data, "flux_fit")])
 
     # meta data
     if meta:
@@ -181,7 +182,7 @@ def aspcapStar_read(fp, meta=False, verbose=False):
 
 
 def test_aspcapStar_read():
-    fp = '/pool/sdss/apogee_dr13/aspcapStar-r6-l30e.2-2M07332578+2044059.fits'
+    fp = "/pool/sdss/apogee_dr13/aspcapStar-r6-l30e.2-2M07332578+2044059.fits"
     spec = aspcapStar_read(fp, True)
     spec.pprint()
     print(spec.meta)
@@ -190,7 +191,7 @@ def test_aspcapStar_read():
 
 
 def test_apStar_read():
-    fp = '/pool/sdss/apogee_dr13/apStar-r6-VESTA.fits'
+    fp = "/pool/sdss/apogee_dr13/apStar-r6-VESTA.fits"
     spec = apStar_read(fp, True)
     spec.pprint()
     print(spec.meta)
@@ -206,7 +207,7 @@ def apStar_url(telescope, location_id, field, file_,
     Parameters
     ----------
     telescope: string
-        TELESCOPE, {'apo1m', 'apo25m'}
+        TELESCOPE, {"apo1m', 'apo25m'}
     location_id: int
         for 'apo1m', it's 1
         for 'apo25m', it's like PLATE
@@ -230,7 +231,7 @@ def apStar_url(telescope, location_id, field, file_,
         currently it's 'r6' @20161031
     """
 
-    if url_header is None or url_header is 'sas':
+    if url_header is None or url_header is "sas":
         url_header = ("https://data.sdss.org/sas/dr13/apogee"
                       "/spectro/redux/r6/stars")
 
@@ -239,10 +240,10 @@ def apStar_url(telescope, location_id, field, file_,
     field = field.strip()
     file_ = file_.strip()
 
-    if telescope == 'apo1m':
+    if telescope == "apo1m":
         # apo1m
         url = "%s/%s/%s/%s" % (url_header, telescope, field, file_)
-    elif telescope == 'apo25m':
+    elif telescope == "apo25m":
         # apo25m
         url = "%s/%s/%s/%s" % (url_header, telescope, location_id, file_)
     else:
@@ -277,7 +278,7 @@ def aspcapStar_url(location_id, file_, url_header=None):
         currently it's 'r6' @20161031
     """
 
-    if url_header is None or url_header is 'sas':
+    if url_header is None or url_header is "sas":
         url_header = ("https://data.sdss.org/sas/dr13/apogee"
                       "/spectro/redux/r6/stars/l30e/l30e.2")
 
@@ -292,7 +293,8 @@ def aspcapStar_url(location_id, file_, url_header=None):
     return url
 
 
-def apStar_download(url, file_path, verbose=False):
+def apStar_download(url, file_path, verbose=False,
+                    username="sdss", password="2.5-meters"):
     """ apStar file downloading utils
     which in principle is able to download everything from a valid url
 
@@ -311,19 +313,36 @@ def apStar_download(url, file_path, verbose=False):
         True if success.
 
     """
+
+    # # if exists, do nothing
+    # if os.path.exists(file_path):
+    #     try:
+    #         fits.open(file_path)
+    #         return True
+    #     except Exception:
+    #         pass
+
+    # credentials for sdss
+    p = urllib.request.HTTPPasswordMgrWithDefaultRealm()
+    p.add_password(None, url, username, password)
+    handler = urllib.request.HTTPBasicAuthHandler(p)
+    opener = urllib.request.build_opener(handler)
+    urllib.request.install_opener(opener)
+
+    # request data
     status = True
     try:
-        response = urllib.urlopen(url)
-        f = open(file_path, 'w+')
-        f.write(response.read())
-        f.close()
-    except:
+        local_file_path, header = urllib.request.urlretrieve(url, file_path)
+    except Exception:
         status = False
+
+    # verbose
     if verbose:
         if status:
-            print("@Cham: success: %s" % url)
+            print("@Cham: success: {0}".format(url))
         else:
-            print("@Cham: failed for url ""%s""" % url)
+            print("@Cham: failed: {0}".format(url))
+
     return status
 
 
