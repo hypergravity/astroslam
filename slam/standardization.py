48237a301cf4a565755dd4f3c1819def42c7ed49
Bo Zhang <bozhang@nao.cas.cn>
2017-06-17 19:09:12 +0800

    update standardization.py: fixed standardization bug

--- slam/standardization.py
+++ slam/standardization.py
@@ -47,18 +47,39 @@ def standardize(X, weight=None, robust=False):
 
     """
     if weight is None:
-        weight = np.ones_like(X, bool)
+        weight = np.ones_like(X, int)
+    weight = np.logical_and(weight > 0, np.isfinite(X))
+
+    ind_good = weight > 0
+    n_good = np.sum(ind_good, axis=0)
+
+    scaler = preprocessing.StandardScaler()
+    n_col = X.shape[1]
+
+    scaler.scale_ = np.ones((n_col,), float)
+    scaler.mean_ = np.zeros((n_col,), float)
 
     if robust:
-        scaler = preprocessing.StandardScaler()
-        X_ = np.where(weight > 0, X, np.nan)
-        scaler.scale_ = (np.diff(np.nanpercentile(X_, (16, 84), axis=0),
-                                 axis=0) / 2.).flatten()
-        scaler.scale_ = np.where(scaler.scale_ <= 0, 1., scaler.scale_)
-        scaler.mean_ = np.nanmedian(X_, axis=0)
-    else:
-        scaler = preprocessing.StandardScaler().fit(X)
+        # estimate using percentiles
+        for i_col in range(n_col):
+            if n_good[i_col] > 0:
+                # at least 1 good pixels
+                scaler.mean_[i_col] = (np.nanpercentile(
+                    X[ind_good[:, i_col], i_col], 84) + np.nanpercentile(
+                    X[ind_good[:, i_col], i_col], 16)) / 2.
+                scaler.scale_[i_col] = (np.nanpercentile(
+                    X[ind_good[:, i_col], i_col], 84) - np.nanpercentile(
+                    X[ind_good[:, i_col], i_col], 16)) / 2.
 
+    else:
+        # estimate using mean and std
+        for i_col in range(n_col):
+            if n_good[i_col] > 0:
+                # at least 1 good pixels
+                scaler.scale_[i_col] = np.std(X[ind_good[:, i_col], i_col])
+                scaler.mean_[i_col] = np.mean(X[ind_good[:, i_col], i_col])
+
+    scaler.scale_ = np.where(scaler.scale_ < 1e-300, 1., scaler.scale_)
     scaler.robust = robust
     X_scaled = scaler.transform(X)
     return scaler, X_scaled
