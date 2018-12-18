9ea000536bc6dba9c7ab45c72073ea46928b33bb
hypergravity <bozhang123>
2018-12-18 12:21:10 +0800

    version 1.2018.1218.0

--- setup.py
+++ setup.py
@@ -1,7 +1,3 @@
-# from distutils.core import setup
-# from .slam import __version__
-
-# if __name__ == '__main__':
 import setuptools
 
 with open("README.md", "r") as fh:
@@ -9,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='astroslam',
-    version="1.2018.1212.0",
+    version="1.2018.1218.0",
     author='Bo Zhang',
     author_email='bozhang@nao.cas.cn',
     description=('A forward model using SVR to estimate stellar parameters'
@@ -19,29 +15,17 @@ setuptools.setup(
     url='http://github.com/hypergravity/astroslam',
     packages=setuptools.find_packages(),
     license='MIT',
-    # install_requires=['numpy>=1.7','scipy','matplotlib','nose'],
-
-    classifiers=[
-        "Development Status :: 6 - Mature",
-        "Intended Audience :: Science/Research",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        # "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.5",
-        "Topic :: Scientific/Engineering :: Astronomy",
-        "Topic :: Scientific/Engineering :: Physics"],
+    classifiers=["Development Status :: 5 - Production/Stable",
+                 "Intended Audience :: Science/Research",
+                 "License :: OSI Approved :: MIT License",
+                 "Operating System :: OS Independent",
+                 "Programming Language :: Python :: 3.7",
+                 "Topic :: Scientific/Engineering :: Physics",
+                 "Topic :: Scientific/Engineering :: Astronomy"],
     package_dir={'slam': 'slam',
                  'extern': 'slam/extern'},
     package_data={'slam': ['data/*.csv']},
     # include_package_data=True,
-    requires=['numpy',
-              'scipy',
-              'matplotlib',
-              'astropy',
-              'sklearn',
-              'joblib',
-              'pandas',
-              'emcee',
-              'lmfit',
-              'ipyparallel']
+    requires=['numpy', 'scipy', 'matplotlib', 'astropy', 'sklearn', 'joblib',
+              'pandas', 'emcee', 'lmfit', 'ipyparallel']
 )
