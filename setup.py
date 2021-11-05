a868575509218b3e858328d2773c0eb54c70c261
Bo Zhang <bozhang@nao.cas.cn>
2021-11-05 17:24:27 +0800

    version 1.2021.1105.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='astroslam',
-    version="1.2020.1202.0",
+    version="1.2021.1105.0",
     author='Bo Zhang',
     author_email='bozhang@nao.cas.cn',
     description=('A forward model using SVR to estimate stellar parameters'
